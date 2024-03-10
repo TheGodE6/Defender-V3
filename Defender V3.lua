761df9b606778c3d88e85fdb5314ef9cacb5c6ac
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-10 18:25:44 +0000

    Update and rename Defender V3 to Defender V3.lua

--- Defender V3.lua
+++ Defender V3.lua
@@ -1,6 +1,3 @@
-local prefix = “>”
-local Scrowner = "dawninja21"
-
 local function notify(msg)
 	game.StarterGui:SetCore("SendNotification", {
 		Title = "DEFENDER V3",
@@ -9,8 +6,31 @@ local function notify(msg)
 	})
 end;
 
--- Credits to asechka19872(simplekah, simplekah v2 owner)
+-- Credits to asechka19872 (simplekah, simplekah v2 owner) and ts2021 (kohlslite owner) --
 
+-- VARIABLES --
+local antikill = true
+local localPlayer = game.Players.LocalPlayer
+local prefix = “>”
+local Scrowner = "dawninja21"
+
+--------------MESSAGE----------------
+notify("🛡️Defender V3🛡️ Loaded.")
+
+-------------CUSTOM CMDS----------------
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	task.wait(0)
+		
+    	if string.sub(msg:lower(), 1, #prefix + 8) == prefix.."antikill" then
+           antikill = true
+        end
+		
+        if string.sub(msg:lower(), 1, #prefix + 10) == prefix.."unantikill" then
+           antikill = false
+        end
+end)
+
+-- MODULES --
 local function onPlayerAdded(player)
     game.Players:Chat("h \n\n\n " .. player.Name .. " \n\n\n Age:"..player.AccountAge.." \n\n\n has joined the game. 🛡️DEFENDER V3🛡️ \n\n\n")
 end
@@ -22,18 +42,12 @@ end
 game.Players.PlayerAdded:Connect(onPlayerAdded)
 game.Players.PlayerRemoving:Connect(onPlayerLeaving)
 
---------------MESSAGE----------------
-notify("🛡️Defender V3🛡️Loaded.")
-
--------------CUSTOM CMDS----------------
-
 --------------AUTO CMDS----------------
-local localPlayer = game.Players.LocalPlayer
 
 task.spawn(function()
     while true do
         task.wait()
-                if localPlayer.Character.Humanoid.Health == 0 then
+                if localPlayer.Character.Humanoid.Health == 0 and antikill == true then
                             Chat("reset me")
                 end
     end
